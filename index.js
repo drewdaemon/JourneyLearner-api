1f562b99b8d1e37b640e2059c4a6638be235ab7b
Andrew Tate <drewctate@gmail.com>
2016-05-04 11:42:59 -0500

    Enabling CORS

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var MongoClient = require('mongodb').MongoClient;
 var port = 2000;
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/maps', function (req, res) {
   MongoClient.connect('mongodb://localhost:27017/journeylearner', function(err, db) {
     if (err) {
