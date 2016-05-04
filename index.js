45a58e453037e7ffc3172b2d1cf72a4a613d4dbd
Andrew Tate <drewctate@gmail.com>
2016-05-04 11:36:41 -0500

    Adding basic mongo access and endpoint

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var port = 2000;
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/maps', function (req, res) {
+  MongoClient.connect('mongodb://localhost:27017/journeylearner', function(err, db) {
+    if (err) {
+      throw err;
+    }
+    db.collection('maps').find().toArray(function(err, result) {
+      if (err) {
+        throw err;
+      }
+      console.log('Get request to maps API: /maps');
+      res.send(result);
+    });
+  });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Maps API listening on port ' + port + '!');
 });
