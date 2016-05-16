f563e6810612a602780a3dae4adefd15b5c2d695
Andrew Tate <drewctate@gmail.com>
2016-05-16 16:26:34 -0500

    Adding S3 uploading

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var aws = require('aws-sdk');
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 var app = express();
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 var mapSchema = mongoose.Schema({
   id: Number,
@@ -55,11 +60,37 @@ app.get('/maps', function (req, res) {
   });
 });
 
+
 app.post('/maps', function (req, res) {
   var map = new Map(req.data);
   map.save(function (err) {
     if (err) return handleError(err);
     console.log('Saved ' + map.title + ' by ' + map.author);
+  });
+});
+
+app.get('/sign_s3', function(req, res){
+  aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+  var s3 = new aws.S3();
+  var s3_params = {
+    Bucket: S3_BUCKET,
+    Key: req.query.file_name,
+    Expires: 60,
+    ContentType: req.query.file_type,
+    ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3_params, function(err, data){
+    if(err){
+      console.log(err);
+    }
+    else{
+      var return_data = {
+        signed_request: data,
+        url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+      };
+      res.write(JSON.stringify(return_data));
+      res.end();
+    }
   });
 });
 
